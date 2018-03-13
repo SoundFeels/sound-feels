6d3b7ba25e55aa0e6590bc1cb5deecfe2747e140
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2018-03-13 09:51:18 -0700

    Added functioning popover

--- make/script.js
+++ make/script.js
@@ -8,3 +8,8 @@ letsPlay.addEventListener('click', function(){
 
 closeBtn.addEventListener('click', function(){
     gamesPopover.style.display='none';})
+
+var gamesCanvas = document.getElementById('games-canvas');
+
+gameBtn1.addEventListener('click', function(){
+    gamesCanvas.style.background='none';})
