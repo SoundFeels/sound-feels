fc0e2643f231e84b2a9b6e3e1c1e24b8a22432ae
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2018-03-13 11:59:34 -0700

    Centered the Canvas

--- make/script.js
+++ make/script.js
@@ -2,7 +2,15 @@ var gamesPopover = document.getElementById('games-popover');
 var letsPlay = document.getElementById('lets-play');
 var closeBtn = document.getElementById('close-btn');
 
+function setup() { //once
+    var gamesCanvas = createCanvas(800, 400); //capture the createCanvas to a variable so you can infuence it with CSS
+    gamesCanvas.parent('games-canvas'); //parent the myCanvas var to the mySketch id for CSS
+    background('#FDB4B5'); //color
+
+}
+
 letsPlay.addEventListener('click', function() {
+    gamesPopover.style.display = 'block';
 
 })
 
