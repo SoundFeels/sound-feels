8944939d946589b3807de1ddf91b61ea352f9fb6
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2018-03-13 09:54:01 -0700

    Added p5 audio and video

--- make/script.js
+++ make/script.js
@@ -13,3 +13,42 @@ var gamesCanvas = document.getElementById('games-canvas');
 
 gameBtn1.addEventListener('click', function(){
     gamesCanvas.style.background='none';})
+
+
+
+
+
+//Using loadPixels to edit the image with the microphone levels.
+'use strict'
+
+var capture;
+var button;
+var playing = false;
+var mic;
+
+function setup() {
+  createCanvas(600, 400);
+  capture = createCapture(VIDEO);
+  capture.size(600, 400);
+  capture.hide();
+  mic = new p5.AudioIn();
+  mic.start();
+
+}
+
+function draw() {
+  background(0);
+  var micLevel = mic.getLevel();
+  capture.loadPixels();
+
+  //draw the matrix of pixels and distorbe them with the mic.
+  var stepSize = round(constrain(micLevel*height*0.6, 5, 400));
+  for (var y=0; y<height; y+=stepSize) {//y axis
+    for (var x=0; x<width; x+=stepSize) {//x axis
+      var i = y * width + x;
+      var darkness = (255 - capture.pixels[i*4]) / 255;
+      var radius = stepSize * darkness;
+      ellipse(x, y, radius, radius); //edit each pixel
+    }
+  }
+}
