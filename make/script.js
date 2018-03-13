f581156b7b6d4799c967163d7ec3ba3015b384db
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2018-03-13 12:57:24 -0700

    Games and game buttons now work!

--- make/script.js
+++ make/script.js
@@ -2,18 +2,77 @@ var gamesPopover = document.getElementById('games-popover');
 var letsPlay = document.getElementById('lets-play');
 var closeBtn = document.getElementById('close-btn');
 
+// show popover
+letsPlay.addEventListener('click', function() {
+    gamesPopover.style.display = 'block';
+})
+
+// hide popover
+closeBtn.addEventListener('click', function() {
+    gamesPopover.style.display = 'none';
+})
+
+// bring buttons into js
+var gmbtn1 = document.getElementById('game-button-1');
+var gmbtn2 = document.getElementById('game-button-2');
+var gmbtn3 = document.getElementById('game-button-3');
+
+// event listen for clicks on game buttons
+var game1;
+var game2;
+var game3;
+gmbtn1.addEventListener('click', function(){
+    game1 = true;
+    game2 = false;
+    game3 = false;
+})
+gmbtn2.addEventListener('click', function(){
+    game1 = false;
+    game2 = true;
+    game3 = false;
+})
+gmbtn3.addEventListener('click', function(){
+    game1 = false;
+    game2 = false;
+    game3 = true;
+})
+
 function setup() { //once
     var gamesCanvas = createCanvas(800, 400); //capture the createCanvas to a variable so you can infuence it with CSS
     gamesCanvas.parent('games-canvas'); //parent the myCanvas var to the mySketch id for CSS
     background('#FDB4B5'); //color
-
+    //Experimental
+    capture = createCapture(VIDEO);
+    capture.size(600, 400);
+    capture.hide();
+    mic = new p5.AudioIn();
+    mic.start();
 }
 
-letsPlay.addEventListener('click', function() {
-    gamesPopover.style.display = 'block';
+function draw() {
 
-})
+    if (game1 == true) {
+        background('#bb0000');
+    }
 
-closeBtn.addEventListener('click', function() {
-    gamesPopover.style.display = 'none';
-})
+    if (game2 == true) {
+        background('#b00000');
+    }
+
+    if (game3 == true) {
+        background(0);
+        var micLevel = mic.getLevel();
+        capture.loadPixels();
+
+        //draw the matrix of pixels and distorbe them with the mic.
+        var stepSize = round(constrain(micLevel * height * 0.6, 5, 400));
+        for (var y = 0; y < height; y += stepSize) { //y axis
+            for (var x = 0; x < width; x += stepSize) { //x axis
+                var i = y * width + x;
+                var darkness = (255 - capture.pixels[i * 4]) / 255;
+                var radius = stepSize * darkness;
+                ellipse(x, y, radius, radius); //edit each pixel
+            }
+        }
+    }
+}
