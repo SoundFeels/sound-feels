713c6dc8ff772cbbcf457e9ffa52528de94d37d8
turner-c <35343261+turner-c@users.noreply.github.com>
2018-03-13 11:49:31 -0700

    Update to Listen
    Continue working on overlays and buttons.

--- listen/script.js
+++ listen/script.js
@@ -10,6 +10,48 @@ var blueNoise = document.getElementById('blueNoise');
 var violetNoise = document.getElementById('violetNoise');
 var greyNoise = document.getElementById('greyNoise');
 
+// get variables for the buttons in white Overlay
+var pinkNoiseWhiteOver = document.getElementById('pinkNoiseWhiteOver');
+var brownNoiseWhiteOver = document.getElementById('brownNoise');
+var blueNoiseWhiteOver = document.getElementById('blueNoise');
+var violetNoiseWhiteOver = document.getElementById('violetNoise');
+var greyNoiseWhiteOver = document.getElementById('greyNoise');
+
+// get variables for the buttons in Pink Overlay
+ var whiteNoisePinkOver = document.getElementById('whiteNoisePinkOver');
+ var brownNoisePinkOver = document.getElementById('brownNoisePinkOver');
+var blueNoisePinkOver = document.getElementById('blueNoisePinkOver');
+var violetNoisePinkOver = document.getElementById('violetNoisePinkOver');
+var greyNoisePinkOver = document.getElementById('greyNoisePinkOver');
+
+// // get variables for the buttons in Brown Overlay
+// var pinkNoiseWhiteOver = document.getElementById('pinkNoiseWhiteOver');
+// var brownNoiseWhiteOver = document.getElementById('brownNoise');
+// var blueNoiseWhiteOver = document.getElementById('blueNoise');
+// var violetNoiseWhiteOver = document.getElementById('violetNoise');
+// var greyNoiseWhiteOver = document.getElementById('greyNoise');
+
+// get variables for the buttons in Blue Overlay
+// var pinkNoiseWhiteOver = document.getElementById('pinkNoiseWhiteOver');
+// var brownNoiseWhiteOver = document.getElementById('brownNoise');
+// var blueNoiseWhiteOver = document.getElementById('blueNoise');
+// var violetNoiseWhiteOver = document.getElementById('violetNoise');
+// var greyNoiseWhiteOver = document.getElementById('greyNoise');
+
+// get variables for the buttons in Violet Overlay
+// var pinkNoiseWhiteOver = document.getElementById('pinkNoiseWhiteOver');
+// var brownNoiseWhiteOver = document.getElementById('brownNoise');
+// var blueNoiseWhiteOver = document.getElementById('blueNoise');
+// var violetNoiseWhiteOver = document.getElementById('violetNoise');
+// var greyNoiseWhiteOver = document.getElementById('greyNoise');
+
+// get variables for the buttons in Grey Overlay
+// var pinkNoiseWhiteOver = document.getElementById('pinkNoiseWhiteOver');
+// var brownNoiseWhiteOver = document.getElementById('brownNoise');
+// var blueNoiseWhiteOver = document.getElementById('blueNoise');
+// var violetNoiseWhiteOver = document.getElementById('violetNoise');
+// var greyNoiseWhiteOver = document.getElementById('greyNoise');
+
 // get variables for ovelays
 var whiteOverlay = document.getElementById('whiteOverlay');
 var pinkOverlay = document.getElementById('pinkOverlay');
@@ -27,57 +69,107 @@ var closeViolet = document.getElementById('closeViolet');
 var closeGrey = document.getElementById('closeGrey');
 
 //open overlays
-whiteNoise.addEventListener('click', function(){
- whiteOverlay.style.display='block';
- closeWhite.style.display='block';
+whiteNoise.addEventListener('click', function() {
+    whiteOverlay.style.display = 'block';
+    closeWhite.style.display = 'block';
 });
 
-pinkNoise.addEventListener('click', function(){
- pinkOverlay.style.display='block';
- closePink.style.display='block';
+pinkNoise.addEventListener('click', function() {
+    pinkOverlay.style.display = 'block';
+    closePink.style.display = 'block';
 });
 
-brownNoise.addEventListener('click', function(){
- brownOverlay.style.display='block';
- closeBrown.style.display='block';
+brownNoise.addEventListener('click', function() {
+    brownOverlay.style.display = 'block';
+    closeBrown.style.display = 'block';
 });
 
-blueNoise.addEventListener('click', function(){
- blueOverlay.style.display='block';
- closeBlue.style.display='block';
+blueNoise.addEventListener('click', function() {
+    blueOverlay.style.display = 'block';
+    closeBlue.style.display = 'block';
 });
 
-violetNoise.addEventListener('click', function(){
- violetOverlay.style.display='block';
- closeViolet.style.display='block';
+violetNoise.addEventListener('click', function() {
+    violetOverlay.style.display = 'block';
+    closeViolet.style.display = 'block';
 });
 
-greyNoise.addEventListener('click', function(){
- greyOverlay.style.display='block';
- closeGrey.style.display='block';
+greyNoise.addEventListener('click', function() {
+    greyOverlay.style.display = 'block';
+    closeGrey.style.display = 'block';
 });
 
 //close overlays
-closeWhite.addEventListener('click', function(){
-  whiteOverlay.style.display='none';
+closeWhite.addEventListener('click', function() {
+    whiteOverlay.style.display = 'none';
 });
 
-closePink.addEventListener('click', function(){
-  pinkOverlay.style.display='none';
+closePink.addEventListener('click', function() {
+    pinkOverlay.style.display = 'none';
 });
 
-closeBrown.addEventListener('click', function(){
-  brownOverlay.style.display='none';
+closeBrown.addEventListener('click', function() {
+    brownOverlay.style.display = 'none';
 });
 
-closeBlue.addEventListener('click', function(){
-  blueOverlay.style.display='none';
+closeBlue.addEventListener('click', function() {
+    blueOverlay.style.display = 'none';
 });
 
-closeViolet.addEventListener('click', function(){
-  violetOverlay.style.display='none';
+closeViolet.addEventListener('click', function() {
+    violetOverlay.style.display = 'none';
 });
 
-closeGrey.addEventListener('click', function(){
-  greyOverlay.style.display='none';
+closeGrey.addEventListener('click', function() {
+    greyOverlay.style.display = 'none';
 });
+
+// event listeners for white overlay
+pinkNoiseWhiteOver.addEventListener('click', function() {
+    whiteOverlay.style.display = 'none';
+    pinkOverlay.style.display = 'block';
+    closePink.style.display = 'block';
+});
+
+brownNoiseWhiteOver.addEventListener('click', function() {
+    whiteOverlay.style.display = 'none';
+    brownOverlay.style.display = 'block';
+    closeBrown.style.display = 'block';
+});
+
+blueNoiseWhiteOver.addEventListener('click', function() {
+    whiteOverlay.style.display = 'none';
+    blueOverlay.style.display = 'block';
+    closeBlue.style.display = 'block';
+});
+
+violetNoiseWhiteOver.addEventListener('click', function() {
+    whiteOverlay.style.display = 'none';
+    violetOverlay.style.display = 'block';
+    closeViolet.style.display = 'block';
+});
+
+// // event listeners for Pink overlay
+// whiteNoisePinkOver.addEventListener('click', function() {
+//     pinkOverlay.style.display = 'none';
+//     whiteOverlay.style.display = 'block';
+//     closeWhite.style.display = 'block';
+// });
+//
+// brownNoiseWhiteOver.addEventListener('click', function() {
+//     whiteOverlay.style.display = 'none';
+//     brownOverlay.style.display = 'block';
+//     closeBrown.style.display = 'block';
+// });
+//
+// blueNoiseWhiteOver.addEventListener('click', function() {
+//     whiteOverlay.style.display = 'none';
+//     blueOverlay.style.display = 'block';
+//     closeBlue.style.display = 'block';
+// });
+//
+// violetNoiseWhiteOver.addEventListener('click', function() {
+//     whiteOverlay.style.display = 'none';
+//     violetOverlay.style.display = 'block';
+//     closeViolet.style.display = 'block';
+// });
