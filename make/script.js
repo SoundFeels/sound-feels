3e5dc6e3fd335a3b7f5c06e4de522c14c584b9d0
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2018-03-13 11:39:41 -0700

    Beautify

--- make/script.js
+++ make/script.js
@@ -1,11 +1,11 @@
 var gamesPopover = document.getElementById('games-popover');
-var letsPlay =  document.getElementById('lets-play');
+var letsPlay = document.getElementById('lets-play');
 var closeBtn = document.getElementById('close-btn');
 
-letsPlay.addEventListener('click', function(){
-    gamesPopover.style.display='block';
+letsPlay.addEventListener('click', function() {
+
 })
 
-closeBtn.addEventListener('click', function(){
-    gamesPopover.style.display='none';
+closeBtn.addEventListener('click', function() {
+    gamesPopover.style.display = 'none';
 })
